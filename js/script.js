efbfd3b1bc58f79bd37c3fe8c9a857b483d88936
suryabhan29 <suryabhansingh1029@gmail.com>
2022-11-30 13:11:35 +0530

    today is js day -3

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
 // I  can write js code here
 
+console.log('Hello from external file');
+
+var aa =1;
+
+var bb = 10;
---
+++ js/text.js
@@ -0,0 +1,32 @@
+//1. variable decleration 
+var x; //Statement 1
+
+//2. variable Initialization
+var y; //statement 2
+y = 20; // statement 3 // assigment opretor ki value right to left hoti hain assigment = (=)
+
+
+//3. variable declearation and initialization
+var z = 50; // statement 4
+
+console.log(y + z); // statement 5
+console.log(z - y); // statement 6
+
+
+var c = 5;
+var d = ++c;
+
+console.log('C=' ,c);
+console.log('d=' ,d);
+
+// this is addition assignment operation 
+
+var e = 1; 
+e += 1; // right to left
+
+console.log('e=',e);
+
+var f = 5;
+
+f -= 1; // that is caller subtraction assignment operation
+console.log('f=',f);
